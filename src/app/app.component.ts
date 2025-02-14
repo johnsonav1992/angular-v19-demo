106a5730e31370c675150cc85f10514cefb7bde3
Alex Johnson <johnsonav1992@gmail.com>
2025-02-14 05:43:55 -0600

    Enhance Prettier configuration and update starship display layout for improved styling and accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component, effect, inject, resource, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { RouterOutlet } from '@angular/router';
 import { SwapiService } from './swapi.service';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   private _swapiService = inject(SwapiService);
 
   public starships = rxResource({
-    loader: () => this._swapiService.getAllStarships(),
+    loader: () => this._swapiService.getAllStarships()
   });
 
   // public starships = rxResource({
