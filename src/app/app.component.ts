1679d65e8eeb6cbc8b4ac1411a868bfe0da9b076
Alex Johnson <johnsonav1992@gmail.com>
2025-02-13 21:04:37 -0600

    Refactor starship display and pagination; update Prettier config for consistent formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, inject, resource, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { SwapiService } from './swapi.service';
 import { map } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    private _swapiService = inject(SwapiService);
+  private _swapiService = inject(SwapiService);
 
-    public starships = rxResource({
-        loader: () => this._swapiService.getAllStarships().pipe(map((res) => res.results)),
-    });
+  // public starships = rxResource({
+  //   request: () => ({ pageNum: this.starshipsPage() }),
+  //   loader: ({ request: { pageNum } }) =>
+  //     this._swapiService.getAllStarships(pageNum),
+  // });
+
+  public starships = rxResource({
+    loader: () => this._swapiService.getAllStarships(),
+  });
+
+  // public vehicles = resource({
+  //   loader: () =>
+  //     fetch(`${this._swapiService.baseUrl}/vehicles`).then((res) =>
+  //       res.json().then((res) => res.results),
+  //     ),
+  // });
+
+  public starshipsPage = signal(1);
+
+  public goToNextPage() {
+    if (this.starships.value()?.next) {
+      this.starshipsPage.update((page) => page + 1);
+    }
+  }
+
+  public goToPreviousPage() {
+    if (this.starships.value()?.previous) {
+      this.starshipsPage.update((page) => page - 1);
+    }
+  }
 }
