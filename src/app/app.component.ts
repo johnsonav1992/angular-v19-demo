9cf1b14cc6b8b05d9d31506b1b902bba27b337a6
Alex Johnson <johnsonav1992@gmail.com>
2025-02-15 03:11:10 -0600

    Refactor app component to enhance layout and add ship selection functionality with linkedSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component, effect, inject, resource, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  linkedSignal,
+  resource,
+  signal
+} from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { SwapiService } from './swapi.service';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [FormsModule]
 })
 export class AppComponent {
   private _swapiService = inject(SwapiService);
 
-  public starships = rxResource({
-    loader: () => this._swapiService.getAllStarships()
-  });
-
   // public starships = rxResource({
-  //   request: () => ({ pageNum: this.starshipsPage() }),
-  //   loader: ({ request: { pageNum } }) =>
-  //     this._swapiService.getAllStarships(pageNum)
+  //   loader: () => this._swapiService.getAllStarships()
   // });
+
+  public starships = rxResource({
+    request: () => ({ pageNum: this.starshipsPage() }),
+    loader: ({ request: { pageNum } }) =>
+      this._swapiService.getAllStarships(pageNum)
+  });
 
   // public vehicles = resource({
   //   loader: () =>
@@ -28,6 +38,24 @@ export class AppComponent {
   // });
 
   public starshipsPage = signal(1);
+
+  public selectableShips = computed(() => {
+    return this.starships.value()?.results.map((ship) => ship.name);
+  });
+
+  public selectedShip = linkedSignal<string[] | undefined, string>({
+    source: this.selectableShips,
+    computation: (newStarshipsVal, previousState) => {
+      if (
+        !newStarshipsVal ||
+        newStarshipsVal.includes(String(previousState?.value))
+      ) {
+        return String(previousState?.value);
+      }
+
+      return String(newStarshipsVal[0]);
+    }
+  });
 
   public goToNextPage() {
     this.starshipsPage.update((page) => page + 1);
